7455850646db1eb3c7242899bd86c9d0d937877e
James Rock <hello@jamesrock.me>
2018-06-19 22:05:13 +0100

    updates

--- js/blinker.js
+++ js/blinker.js
@@ -1,29 +1,28 @@
 (function() {
 
 	var
-	input = prompt('enter text'),
-	input = input.replace(/ /g, ' '),
+	input = (prompt('enter text') + ' '),
 	split = input.split(''),
 	interval,
-	time = (0.5*1000),
+	time = (0.2*1000),
 	render = function() {
-		app.innerHTML = split.join('');
+		app.innerHTML = split.join('').replace(/ /g, '&nbsp;');
 	};
 
 	/*
 
 	why hello, my name is james!
+	you're a complete idiot!
 
 	*/
 
 	interval = setInterval(function() {
 
-		// console.log(split);
 		render();
 		split.unshift(split.pop());
 
 	}, time);
 
-	console.log('input', input);
+	// console.log('input', input);
 
 })();
