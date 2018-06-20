6202c0df7e3fb45ab70dba983319fc7d75ba9b37
James Rock <hello@jamesrock.me>
2018-06-20 05:54:54 +0100

    updates

--- js/blinker.js
+++ js/blinker.js
@@ -1,27 +1,34 @@
 (function() {
 
+	// console.log('bob', app.clientHeight/app.clientWidth);
+
 	var
-	input = (prompt('enter text') + ' '),
+	input = prompt('enter text'),
+	input = (input.replace(/./g, ' ') + input),
 	split = input.split(''),
 	interval,
 	time = (0.2*1000),
 	render = function() {
-		app.innerHTML = split.join('').replace(/ /g, '&nbsp;');
+		var string = split.join('').replace(/ /g, '&nbsp;');
+		app.innerHTML = string;
 	};
 
 	/*
 
 	why hello, my name is james!
 	you're a complete idiot!
+	so you're thinking of changing the name of this to ticker?
 
 	*/
 
 	interval = setInterval(function() {
 
 		render();
-		split.unshift(split.pop());
+		split.push(split.shift());
 
 	}, time);
+
+	// clearInterval(interval);
 
 	// console.log('input', input);
 
